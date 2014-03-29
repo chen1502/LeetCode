ca081cab6168a2f2a12ba66fa75757676884e92b
Yongqiang Chen <chern1502@gmail.com>
2014-03-28 21:37:26 -0500

    Update binaryTreeInorderTraversal.java

--- binaryTreeInorderTraversal.java
+++ binaryTreeInorderTraversal.java
@@ -20,3 +20,64 @@ public class Solution {
         go(node.right, list);
     }
 }
+
+
+
+
+
+
+// Iterative with a stack
+public class Solution {
+ 
+    public ArrayList<Integer> inorderTraversal(TreeNode root) {
+         ArrayList<Integer> list = new ArrayList<Integer>();
+         Stack<TreeNode> S = new Stack<TreeNode>();
+         TreeNode curr = root;
+         while(true) {
+              if(curr != null) {
+                   S.push(curr);
+                   curr = curr.left;
+              }
+              else if (!S.empty()) {
+                   curr = S.pop();
+                   list.add(curr.val);
+                   curr = curr.right;
+              }
+              else break;
+         }
+         return list;
+    }
+}
+
+
+
+
+
+// Iterative without a stack. Threaded binary tree
+public class Solution {
+ 
+     public ArrayList<Integer> inorderTraversal(TreeNode root) {
+          ArrayList<Integer> list = new ArrayList<Integer>();
+          TreeNode curr = root;
+          while(curr != null) {
+               if (curr.left != null) {
+                    TreeNode temp = curr.left;
+                    while(temp.right != null && temp.right != curr) temp = temp.right;
+                    if (temp.right == null) {
+                         temp.right = curr;
+                         curr = curr.left;
+                    }
+                    else {
+                         temp.right = null;
+                         list.add(curr.val);
+                         curr = curr.right;
+                    }
+               }
+               else {
+                    list.add(curr.val);
+                    curr = curr.right;
+               }
+          }
+          return list;
+     }
+}
